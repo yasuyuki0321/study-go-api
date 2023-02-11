eb40afd71093ff379e068a561e4e12cfaae3813d
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2023-02-12 00:21:52 +0900

    feat: atricleエンドポイントを作成

--- main.go
+++ main.go
@@ -13,7 +13,12 @@ func main() {
 		fmt.Printf("%v\n", r.Header.Get("foo"))
 	}
 
+	postArticleHandler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
 
 	log.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
