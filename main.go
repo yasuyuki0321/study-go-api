80d1dc9b659e21d1ecd641f16656331ab2b2edf5
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2023-02-12 00:19:00 +0900

    fix: handler名のtypoを修正

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
-	hellHandler := func(w http.ResponseWriter, r *http.Request) {
+	helloHandler := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello World!!\n")
 		fmt.Printf("%v\n", r.Header.Get("foo"))
 	}
 
-	http.HandleFunc("/hello", hellHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
