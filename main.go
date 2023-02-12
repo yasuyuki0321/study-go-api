2f9ea08227a06966bc1dd3ce5d4721b2dfa8bfb3
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2023-02-12 16:01:51 +0900

    feat: エンドポイントを追加

--- main.go
+++ main.go
@@ -17,8 +17,30 @@ func main() {
 		io.WriteString(w, "Posting Article...\n")
 	}
 
+	articleListHandler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+
+	articleDetailHandler := func(w http.ResponseWriter, r *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("Article No %d\n", articleID)
+		io.WriteString(w, resString)
+	}
+
+	postNiceHandler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	postCommentHandler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleDetailHandler)
+	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
 
 	log.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
